c340f77c41d26a1d3ffb0dbca2ac24c9e6b3dfc7
Кирилл Ковалев <seokirillstarsnab@gmail.com>
2017-09-09 09:17:57 +0300

    Логирование в консоль

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+import { createLogger } from 'redux-logger';
+
+const logger = createLogger({
+    diff: true,
+    duration: true,
+});
+
+addMiddleware(logger);
+
 export default function DiffState() {
     return <div>Hello, DiffState!</div>;
 };
